3b373dc648d3df3afa52db3689ad573bdb5ddcc4
José Carreño <jose.carreno@somosoh.pe>
2019-06-28 19:41:09 -0500

    Inicial

--- DemoRSA/ViewController.swift
+++ DemoRSA/ViewController.swift
@@ -7,12 +7,36 @@
 //
 
 import UIKit
+import SwiftyRSA
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        do {
+            let certificateData = NSData(contentsOf:Bundle.main.url(forResource: "public", withExtension: "der")!)
+            
+            let certificate = SecCertificateCreateWithData(nil, certificateData!)
+            
+            var trust: SecTrust?
+            
+            let policy = SecPolicyCreateBasicX509()
+            let status = SecTrustCreateWithCertificates(certificate!, policy, &trust)
+            
+            if status == errSecSuccess {
+                let key = SecTrustCopyPublicKey(trust!)!
+                
+                let publicKey = try PublicKey(reference: key)
+                let clear = try ClearMessage(string: "Clear Text", using: .utf8)
+                let encrypted = try clear.encrypted(with: publicKey, padding: .OAEP)
+    
+                print(encrypted.base64String)
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+
     }
 
 
